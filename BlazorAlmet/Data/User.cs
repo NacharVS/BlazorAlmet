bfa4965823ad726a7572472059cb38df97b1756d
nacharvs <r3dinc@yandex.ru>
2023-06-20 08:43:23 +0300

    ListOfUsersExStart

---
+++ BlazorAlmet/Data/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorAlmet.Data
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public bool IsDriver { get; set; }
+        public string Vacancy { get; set; }
+    }
+}
